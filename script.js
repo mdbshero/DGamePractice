29c594a5a3f48b195c85da7f9da7ac31a549ed97
Mdbshero <mdbshero@gmail.com>
2022-07-18 14:06:34 -0500

    added win counter.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //First dice random number and image
 
 let dice = document.querySelectorAll("img");
+let userOneWins = 0;
+let userTwoWins = 0;
 function roll() {
   dice.forEach(function (die) {
     die.classList.add("shake");
@@ -10,22 +12,22 @@ function roll() {
       die.classList.remove("shake");
     });
     const firstRandomNum = Math.floor(Math.random() * 6) + 1;
-
     const firstDiceImage = `assets/dice${firstRandomNum}.png`;
-
     document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
 
     //Second dice random number and image
     const secondRandomNum = Math.floor(Math.random() * 6) + 1;
-
     const secondDiceImage = `assets/dice${secondRandomNum}.png`;
-
     document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
 
     if (firstRandomNum > secondRandomNum) {
+      userOneWins++;
       document.querySelector("h1").innerHTML = "The Winner is User 1!";
+      document.querySelectorAll("h4")[0].innerHTML = "Wins: " + userOneWins;
     } else if (firstRandomNum < secondRandomNum) {
+      userTwoWins++;
       document.querySelector("h1").innerHTML = "The Winner is User 2!";
+      document.querySelectorAll("h4")[1].innerHTML = "Wins: " + userTwoWins;
     } else {
       document.querySelector("h1").innerHTML = "Draw!";
     }
