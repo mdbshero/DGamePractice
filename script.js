f68a3e07de0636b0b469d1e6424a90840eb9428d
Mdbshero <mdbshero@gmail.com>
2022-07-18 13:49:42 -0500

    moved all JS logic into roll function that only activites on button press.

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 //First dice random number and image
-const firstRandomNum = Math.floor(Math.random() * 6) + 1;
-
-const firstDiceImage = `assets/dice${firstRandomNum}.png`;
-
-document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
-
-//Second dice random number and image
-const secondRandomNum = Math.floor(Math.random() * 6) + 1;
-
-const secondDiceImage = `assets/dice${secondRandomNum}.png`;
-
-document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
-
-if (firstRandomNum > secondRandomNum) {
-  document.querySelector("h1").innerHTML = "The Winner is User 1!";
-} else if (firstRandomNum < secondRandomNum) {
-  document.querySelector("h1").innerHTML = "The Winner is User 2!";
-} else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
 
 let dice = document.querySelectorAll("img");
-console.log(dice)
 function roll() {
   dice.forEach(function (die) {
     die.classList.add("shake");
@@ -30,5 +9,25 @@ function roll() {
     dice.forEach(function (die) {
       die.classList.remove("shake");
     });
+    const firstRandomNum = Math.floor(Math.random() * 6) + 1;
+
+    const firstDiceImage = `assets/dice${firstRandomNum}.png`;
+
+    document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
+
+    //Second dice random number and image
+    const secondRandomNum = Math.floor(Math.random() * 6) + 1;
+
+    const secondDiceImage = `assets/dice${secondRandomNum}.png`;
+
+    document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
+
+    if (firstRandomNum > secondRandomNum) {
+      document.querySelector("h1").innerHTML = "The Winner is User 1!";
+    } else if (firstRandomNum < secondRandomNum) {
+      document.querySelector("h1").innerHTML = "The Winner is User 2!";
+    } else {
+      document.querySelector("h1").innerHTML = "Draw!";
+    }
   }, 1000);
 }
